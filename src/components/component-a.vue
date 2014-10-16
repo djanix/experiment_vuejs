7c86a55f1ea8eefdb37de88e437b3d5e87511f5c
unknown <jbeauchemin@jbeauchemin.ergonet.com>
2014-10-16 16:47:23 -0400

    update browserify script add basic functionality to a component

--- src/components/component-a.vue
+++ src/components/component-a.vue
@@ -11,7 +11,13 @@
     module.exports = {
         data: function () {
             return {
-                msg: 'I am component A!'
+                msg: 'I am component A!',
+                incrementNumber: 0
+            }
+        },
+        methods: {
+            increment: function () {
+                this.$data.incrementNumber++;
             }
         }
     };
@@ -20,5 +26,7 @@
 <template>
     <div id="a">
         <h2>{{msg}}</h2>
+        <button v-on="click:increment()">click me</button>&nbsp;
+        <span>{{incrementNumber}}</span>
     </div>
 </template>
